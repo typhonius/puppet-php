66e4d40766250b0937e1d13ebb92481601513f3a
Peter Rhoades <createdbypete@gmail.com>
2013-10-15 09:06:40 +0100

    Updating composer spec to match new manifest

--- spec/classes/php_composer_spec.rb
+++ spec/classes/php_composer_spec.rb
@@ -10,7 +10,7 @@ describe "php::composer" do
       :command => "curl -sS -o /test/boxen/phpenv/bin/composer http://getcomposer.org/download/1.0.0-alpha7/composer.phar",
       :unless  => "[ -f /test/boxen/phpenv/bin/composer ] && [ \"`md5 -q /test/boxen/phpenv/bin/composer`\" = \"ef51599395560988ea3e16912bfd70f8\" ]",
       :cwd     => "/test/boxen/phpenv",
-      :require => "Exec[phpenv-setup-root-repo]"
+      :require => "Repository[/test/boxen/phpenv]"
     })
 
     should contain_file("/test/boxen/phpenv/bin/composer").with({
