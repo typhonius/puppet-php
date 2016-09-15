b422128473e4ebbcf405ec21d0b31d12e4051fb6
Alex Mace <amace@names.co.uk>
2016-09-15 17:01:32 +0100

    Add the mongodb extension (supersedes the older mongo extension, as per https://pecl.php.net/package/mongo)

---
+++ manifests/extension/mongodb.pp
@@ -0,0 +1,48 @@
+# Installs the mongodb extension for a specific version of php.
+#
+# Usage:
+#
+#     php::extension::mongodb { 'mongodb for 5.4.10':
+#       php     => '5.4.10',
+#       version => '1.4.5'
+#     }
+#
+define php::extension::mongodb(
+  $php,
+  $version = '1.1.8'
+) {
+  require php::config
+
+  # Get full patch version of PHP
+  $patch_php_version = php_get_patch_version($php)
+
+  # Require php version eg. php::5_4_10
+  # This will compile, install and set up config dirs if not present
+  php_require($patch_php_version)
+
+  $extension = 'mongodb'
+  $package_name = "mongodb-${version}"
+  $url = "http://pecl.php.net/get/mongodb-${version}.tgz"
+
+  # Final module install path
+  $module_path = "${php::config::root}/versions/${patch_php_version}/modules/${extension}.so"
+
+  php_extension { $name:
+    extension      => $extension,
+    version        => $version,
+    package_name   => $package_name,
+    package_url    => $url,
+    homebrew_path  => $boxen::config::homebrewdir,
+    phpenv_root    => $php::config::root,
+    php_version    => $patch_php_version,
+    cache_dir      => $php::config::extensioncachedir,
+  }
+
+  # Add config file once extension is installed
+
+  file { "${php::config::configdir}/${patch_php_version}/conf.d/${extension}.ini":
+    content => template('php/extensions/generic.ini.erb'),
+    require => Php_extension[$name],
+  }
+
+}
