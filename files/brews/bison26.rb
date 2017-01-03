792182c01126a65ecd216df7c59ac11f91303111
Adam Malone <adam@adammalone.net>
2017-01-04 09:47:06 +1100

    Updates hash to SHA256

--- files/brews/bison26.rb
+++ files/brews/bison26.rb
@@ -4,7 +4,7 @@ class Bisonphp26 < Formula
   homepage 'http://www.gnu.org/software/bison/'
   url 'http://ftpmirror.gnu.org/bison/bison-2.6.4.tar.gz'
   mirror 'http://ftp.gnu.org/gnu/bison/bison-2.6.4.tar.gz'
-  sha1 '38adec0d7d0f556ec52e21ccbb87edd78327dd9b'
+  sha256 'de2d15dfdcfc24405464cb95acc9d5ef31fb2e5be4aca6a530ec59bb57c30e5d'
 
   version '2.6.4-boxen1'
 
